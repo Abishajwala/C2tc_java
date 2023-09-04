8f35d1a69ac4692247ac2ad062802f475ae8cb33
Abisha Jwala <Abisha Jwala@DESKTOP-OROANE0>
2023-09-05 00:06:19 +0530

    first commit

---
+++ C2tc_java/src/com/abisha/myclass/Classdemo.java
@@ -0,0 +1,14 @@
+package com.abisha.myclass;
+
+public class Classdemo {
+
+	public static void main(String[] args) {
+		// without creating object static methods can be called.
+				MyClass.display();
+				MyClass ob = new MyClass();
+				System.out.println(ob);
+				MyClass.display();
+
+	}
+
+}
