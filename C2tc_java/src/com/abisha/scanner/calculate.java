5ce7f707f1fb48b3a6d19003299b145ad0d07d6b
Abisha Jwala <Abisha Jwala@DESKTOP-OROANE0>
2023-09-05 21:27:58 +0530

    first commit

---
+++ C2tc_java/src/com/abisha/scanner/calculate.java
@@ -0,0 +1,19 @@
+package com.abisha.scanner;
+
+public class calculate {
+	public void calc(person P)
+	{
+		if(P.getIncome()<=16000)
+		{
+			P.setTax(0);
+		}
+		else if(P.getIncome()>16000 && P.getIncome()<= 500000)
+		{
+			P.setTax(5);
+		}
+		else
+		{
+			P.setTax(8);
+		}
+	}
+}
